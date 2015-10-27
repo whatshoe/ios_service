886d4f709aede411b08514a36b8ddf109818dfd1
whatshoe <whatshoe@jeonghun-ui-imac-3.local>
2015-10-27 11:31:04 +0900

    Initial Commit

---
+++ whatshoe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  whatshoe
+//
+//  Created by whatshoe on 2015. 10. 27..
+//  Copyright © 2015년 whatshoe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ whatshoe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  whatshoe
+//
+//  Created by whatshoe on 2015. 10. 27..
+//  Copyright © 2015년 whatshoe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
